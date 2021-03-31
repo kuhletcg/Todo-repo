7f48cdd454189e70b5ec70b17d6fe461de3a6202
TCG <ssibonokuhle@gmail.com>
2021-03-31 13:20:50 +0200

    Added payload that holds the actual data in a redux action object

--- src/redux/actions/food.js
+++ src/redux/actions/food.js
@@ -1,13 +1,15 @@
 import { NUMBER, ADD_FOOD, DELETE_FOOD } from "../actionTypes/food";
 
-export const addFood = (food) => ({
-  type: ADD_FOOD,
-  payload: food,
-});
+export const addFood = (food) => {
+  return {
+    type: ADD_FOOD,
+    payload: food,
+  };
+};
 
 export const deleteFood = (key) => ({
   type: DELETE_FOOD,
-  payload: food,
+  payload: key,
 });
 
 export const ADD_NUMBER = (number) => ({
